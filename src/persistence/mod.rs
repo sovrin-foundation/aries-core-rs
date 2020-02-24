cca107ef6d19bdae3d9f2de9a21680bf3416f486
Cam Parra <caeparra@gmail.com>
2020-02-24 09:32:34 -0700

    fixes bugs and errors in code]
    Signed-off-by: Cam Parra <caeparra@gmail.com>

--- src/persistence/mod.rs
+++ src/persistence/mod.rs
@@ -1,73 +1,235 @@
-use postgres::{Client, NoTls};
-use std::{fmt, env,path::PathBuf};
-use serde::{Serialize, Deserialize};
+/// The errors that can occur during a persistence operation
+pub mod errors;
 
+use async_trait::async_trait;
+use postgres;
+use serde::{Deserialize, Deserializer, Serialize};
+use std::path::PathBuf;
+use tokio_postgres::{tls, Client, Connection, Socket};
 
-
-pub enum PersistenceConfig <A, B>
-where
-    A: AsRef<PathBuf>,
-    B: Into<String>,
-{
-    PostgresStorage(PostgresConfig<A, B>)
+/// General storage configuration options
+pub enum PersistenceConfig {
+    /// Storage configuration used for opening a postgres database
+    PostgresStorage(PostgresConfig),
+    /// Storage configuration used for opening an sqlite database
+    SqliteStorage(SqliteConfig),
 }
 
-
-#[derive(Serialize, Deserialize, Debug)]
-struct PostgreConfig<A: AsRef<PathBuf>, B: Into<String>> {
-    user : Option<B>,
-    password : Option<B>,
-    server : Option<B>,
-    port : Option<B>,
-    name : Option<B>,
+/// Sqlite configuration options
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct SqliteConfig {
+    /// Path to the file. If None, the database will be opened in memory
+    path: Option<PathBuf>,
+    /// The flags to use when opening the sqlite connection
+    flags: SqliteOpenFlags,
 }
 
-trait Connect {
-    fn open(&self) -> Result<String, Error>;
-}
-
-fn reference_string_to_str(userstring : String){
-    if !userstring.is_empty() {
-
+bitflags! {
+    /// Flags for opening Sqlite database connections.
+    #[derive(Serialize, Deserialize)]
+    pub struct SqliteOpenFlags: u16 {
+        /// Open in read only mode
+        const READ_ONLY     = 0x0001;
+        /// Open with read and write permissions
+        const READ_WRITE    = 0x0002;
+        /// Create the database if it doesn't exist
+        const CREATE        = 0x0004;
+        /// Use a URI for the database location
+        const USE_URI       = 0x0008;
+        /// Use an in-memory database
+        const USE_MEMORY    = 0x0010;
+        /// Don't allow mutexes
+        const NO_MUTEX      = 0x0020;
+        /// Allow mutexes during queries
+        const FULL_MUTEX    = 0x0040;
+        /// All connections to the database use the same cache
+        const SHARED_CACHE  = 0x0080;
+        /// All connections to the database independent caches
+        const PRIVATE_CACHE = 0x0100;
     }
 }
 
-impl <A, B>  Connect for PostgreConfig<A, B>
-where
-    A: AsRef<PathBuf>,
-    B: Into<String>,
+impl Default for SqliteOpenFlags {
+    fn default() -> SqliteOpenFlags {
+        SqliteOpenFlags::READ_WRITE
+            | SqliteOpenFlags::CREATE
+            | SqliteOpenFlags::NO_MUTEX
+            | SqliteOpenFlags::USE_URI
+    }
+}
+
+/// Postgres configuration options
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct PostgresConfig {
+    #[serde(deserialize_with = "empty_string_is_none")]
+    user: Option<String>,
+    #[serde(deserialize_with = "empty_string_is_none")]
+    password: Option<String>,
+    #[serde(deserialize_with = "empty_string_is_none")]
+    server: Option<String>,
+    #[serde(deserialize_with = "empty_string_is_none")]
+    port: Option<String>,
+    #[serde(deserialize_with = "empty_string_is_none")]
+    name: Option<String>,
+    #[serde(default, deserialize_with = "empty_string_is_none")]
+    uri: Option<String>,
+}
+
+fn empty_string_is_none<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+    where
+        D: Deserializer<'de>,
 {
-    fn open(&self) {
-
-        let mut postgres_uri : String = "postgresql://".to_string();
-
-        let username= self.name.as_ref().unwrap_or(|| String("postgres"));
-
-        let password = self.password.as_ref().map(|p| format!(":{}", p) )
-            .unwrap_or(String(""));
-
-
-        let sever = self.server.as_ref().as_ref().map(|p| format!(":{}", p))
-            .unwrap_or(String("@localhost"));
+    let s = String::deserialize(deserializer)?;
+    if s.is_empty() {
+        Ok(None)
+    } else {
+        Ok(Some(s))
+    }
+}
 
 
-        let port = self.port.as_ref().map(|p| format!(":{}", p))
-            .unwrap_or(String(""));
+/// Create new connections
+#[async_trait]
+trait Connect {
+    fn create_uri(&mut self) -> Result<Option<String>, errors::PersistenceErrorKind>;
+    fn open(&mut self) -> Result<postgres::Client, errors::PersistenceErrorKind>;
+    async fn async_open(
+        &self,
+    ) -> Result<(Client, Connection<Socket, tls::NoTlsStream>), errors::PersistenceErrorKind>;
+}
 
-        let name = self.name.as_ref().map(|p| format!("/{}", p))
-            .unwrap_or(String(""));
+#[async_trait]
+impl Connect for PostgresConfig {
+    fn create_uri(&mut self) -> Result<Option<String>, errors::PersistenceErrorKind>{
+        let uri = self.uri.as_ref().map_or("", |p| p.as_str());
+        if uri.is_empty() {
+            let mut postgres_uri: String = "postgresql://".to_string();
+            let username = self.user.as_ref().map_or(String::from("postgres"), |s| format!("{}",s));
+            let password = self
+                .password
+                .as_ref()
+                .map_or(String::new(), |p| format!(":{}", p));
+            let sever = self
+                .server
+                .as_ref()
+                .map_or(String::from("@localhost"), |p| format!("@{}", p));
+            let port = self
+                .port
+                .as_ref()
+                .map_or(String::new(), |p| format!(":{}", p));
+            let name = self
+                .name
+                .as_ref()
+                .map_or(String::new(), |p| format!("/{}", p));
 
-        postgres_uri.push_str(username);
+            postgres_uri.push_str(username.as_str());
 
-        if !password.is_empty() {
-            postgres_uri.push_str(password.as_str())
+            if !password.is_empty() {
+                postgres_uri.push_str(&password);
+            }
+
+            postgres_uri.push_str(sever.as_str());
+
+            if !port.is_empty() {
+                postgres_uri.push_str(port.as_str());
+            }
+            if !name.is_empty() {
+                postgres_uri.push_str(name.as_str());
+            }
+            self.uri = Some(postgres_uri.clone());
+            Ok(Some(postgres_uri))
+        } else {
+            Err(errors::PersistenceErrorKind::IOError)
+        }
+    }
+
+    fn open(&mut self) -> Result<postgres::Client, errors::PersistenceErrorKind> {
+        let postgres_uri = self.uri.as_ref().map_or("", |p| p.as_str());
+        if !postgres_uri.is_empty() {
+            let client = postgres::Client::connect(postgres_uri, postgres::NoTls)
+                .map_err(|_| errors::PersistenceErrorKind::IOError)?;
+            Ok(client)
+        } else {
+            let new_uri = self.create_uri()?;
+            let new_uri = new_uri.ok_or(errors::PersistenceErrorKind::IOError)?;
+
+            let client = postgres::Client::connect(new_uri.as_str() , postgres::NoTls)
+                .map_err(|_| errors::PersistenceErrorKind::IOError)?;
+            Ok(client)
+        }
+    }
+
+    async fn async_open(
+        &self,
+    ) -> Result<(Client, Connection<Socket, tls::NoTlsStream>), errors::PersistenceErrorKind> {
+        let postgres_uri = self.uri.as_ref().map_or("", |p| p.as_str());
+        if !postgres_uri.is_empty() {
+            match tokio_postgres::connect(postgres_uri, tokio_postgres::NoTls).await {
+                Ok((client, connection)) => Ok((client, connection)),
+                Err(_) => Err(errors::PersistenceErrorKind::IOError),
+            }
+        } else {
+            Err(errors::PersistenceErrorKind::IOError)
+        }
+    }
+}
+
+#[cfg(test)]
+mod persistence_tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_postgres_config_searlizes() {
+        let demo_config = r#"{"user":"person1","password":"securepassword","server":"127.0.0.1","port":"3976","name":"wallet"}"#;
+        let test_postgres_config_object: PostgresConfig =
+            serde_json::from_str(&demo_config).unwrap();
+        assert_eq!("person1", test_postgres_config_object.user.unwrap())
+    }
+
+    #[test]
+    fn test_postgres_config_searlizes_empty() {
+        let demo_config = r#"{"user":"","password":"","server":"","port":"","name":"", "uri":""}"#;
+        let _test_postgres_config_object: PostgresConfig =
+            serde_json::from_str(&demo_config).unwrap();
+    }
+
+
+    #[test]
+    fn test_postgres_config_() {
+        let correct_postgres_uri =
+            String::from("postgresql://person1:securepassword@127.0.0.1:3976/wallet");
+        let demo_config = r#"{"user":"person1","password":"securepassword","server":"127.0.0.1","port":"3976","name":"wallet","uri":""}"#;
+        let mut test_postgres_config_object: PostgresConfig =
+            serde_json::from_str(demo_config).unwrap();
+        let _conenction = match test_postgres_config_object.create_uri() {
+            Ok(c) => c,
+            Err(e) => panic!("{:?}", e)
+        };
+        assert_eq!(
+            test_postgres_config_object.uri.unwrap(),
+            correct_postgres_uri
+        )
+    }
+
+    #[test]
+    fn test_if_uri_is_not_initialized() {}
+
+    #[test]
+    fn test_open_default_wallet_and_write() {
+        let demo_config = r#"{"user":"","password":"","server":"","port":"","name":"", "uri":""}"#;
+        let mut test_postgres_config_object: PostgresConfig =
+            serde_json::from_str(&demo_config).unwrap();
+        let mut default_client= test_postgres_config_object.open().unwrap();
+        
+        default_client.batch_execute("
+            CREATE TABLE person (
+            id      SERIAL PRIMARY KEY,
+            name    TEXT NOT NULL,
+            data    BYTEA
+            )
+        ").unwrap()
         }
 
 
-
-
-
-
-
-    }
 }
